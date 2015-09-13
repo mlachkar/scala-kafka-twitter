97155ceb61156224c7b60fbc7827ce60f55bc6d9
Meriam Lachkar <meriam.lachkar@gmail.com>
2015-09-14 00:59:29 +0200

    using createDirectStream instead of createStream.

--- src/main/scala/net/mkrcah/KafkaConsumerApp.scala
+++ src/main/scala/net/mkrcah/KafkaConsumerApp.scala
@@ -10,6 +10,13 @@ import org.apache.spark.streaming.StreamingContext._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka._
 
+import com.twitter.bijection.avro.SpecificAvroCodecs.{toJson, toBinary}
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.SparkContext._
+import org.apache.avro.Schema
+import twitter4j.{Status, FilterQuery}
+
 object KafkaConsumerApp extends App{
 
   private val conf = ConfigFactory.load()
@@ -18,20 +25,64 @@ object KafkaConsumerApp extends App{
   val sc = new StreamingContext(sparkConf, Seconds(5))
 
   val encTweets = {
-    val topics = Map(KafkaProducerApp.KafkaTopic -> 1)
-    val kafkaParams = Map(
-      "zookeeper.connect" -> conf.getString("kafka.zookeeper.quorum"),
-      "group.id" -> "1")
-    KafkaUtils.createStream[String, Array[Byte], StringDecoder, DefaultDecoder](
-      sc, kafkaParams, topics, StorageLevel.MEMORY_ONLY)
-  }
+    val topics = KafkaProducerApp.KafkaTopic.split(",").toSet
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> conf.getString("kafka.brokers"))
+    KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder ](sc, kafkaParams, topics)
+}
+
+  val jsonSchema = new Schema.Parser().parse("""{
+                                                   "type":"record",
+                                                   "name":"Tweet",
+                                                   "namespace":"avro",
+                                                   "fields":[
+                                                      {
+                                                         "name":"name",
+                                                         "type":"string"
+                                                      },
+                                                      {
+                                                         "name":"text",
+                                                         "type":"string"
+                                                      }
+                                                   ]
+                                                }"""")
+  val tweets = encTweets.flatMap(x => SpecificAvroCodecs.toJson[Tweet](jsonSchema).invert(x._2).toOption)
+  val hashTags = tweets.flatMap(status =>  status.getText.split(" ").filter(_.startsWith("#")))
 
   val tweets = encTweets.flatMap(x => SpecificAvroCodecs.toBinary[Tweet].invert(x._2).toOption)
 
   val wordCounts = tweets.flatMap(_.getText.split(" ")).map((_,1)).reduceByKey(_ + _)
+
   val countsSorted = wordCounts.transform(_.sortBy(_._2, ascending = false))
 
-  countsSorted.print()
+  val topCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
+                       .map{case (topic, count) => (count, topic)}
+                       .transform(_.sortByKey(false))
+
+  val topCounts10 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(10))
+                       .map{case (topic, count) => (count, topic)}
+                       .transform(_.sortByKey(false))
+
+//	 Print popular hashtags
+
+  topCounts60.foreachRDD(rdd => {
+
+      val topList = rdd.take(10)
+
+      println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
+
+      topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+
+  })
+
+  topCounts10.foreachRDD(rdd => {
+
+        val topList = rdd.take(10)
+
+        println("\nPopular topics in last 10 seconds (%s total):".format(rdd.count()))
+
+        topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+
+      })
 
   sc.start()
   sc.awaitTermination()
--- src/main/scala/net/mkrcah/KafkaProducerApp.scala
+++ src/main/scala/net/mkrcah/KafkaProducerApp.scala
@@ -5,10 +5,13 @@ import java.util.Properties
 import com.twitter.bijection.avro.SpecificAvroCodecs.{toJson, toBinary}
 import com.typesafe.config.ConfigFactory
 import kafka.javaapi.producer.Producer
-import kafka.producer.{KeyedMessage, ProducerConfig}
+import kafka.producer.{DefaultPartitioner, KeyedMessage, ProducerConfig, Partitioner}
 import net.mkrcah.TwitterStream.OnTweetPosted
 import net.mkrcah.avro.Tweet
 import twitter4j.{Status, FilterQuery}
+
+import kafka.serializer.StringEncoder
+import twitter4j.json.DataObjectFactory
 
 object KafkaProducerApp {
 
@@ -20,19 +23,17 @@ object KafkaProducerApp {
     val props = new Properties()
     props.put("metadata.broker.list", conf.getString("kafka.brokers"))
     props.put("request.required.acks", "1")
+    props.put("serializer.class","kafka.serializer.StringEncoder")
+    props.put("partitioner.class", "kafka.producer.DefaultPartitioner")
+    props.put("key.serializer.class", "kafka.serializer.StringEncoder")
     val config = new ProducerConfig(props)
     new Producer[String, Array[Byte]](config)
   }
 
-  val filterUsOnly = new FilterQuery().locations(Array(
-    Array(-126.562500,30.448674),
-    Array(-61.171875,44.087585)))
-
-
   def main (args: Array[String]) {
     val twitterStream = TwitterStream.getStream
     twitterStream.addListener(new OnTweetPosted(s => sendToKafka(toTweet(s))))
-    twitterStream.filter(filterUsOnly)
+    twitterStream.filter(new FilterQuery().track(Array[String]("test")).language(Array[String]("fr")))
   }
 
   private def toTweet(s: Status): Tweet = {
@@ -40,9 +41,10 @@ object KafkaProducerApp {
   }
 
   private def sendToKafka(t:Tweet) {
-    println(toJson(t.getSchema).apply(t))
     val tweetEnc = toBinary[Tweet].apply(t)
-    val msg = new KeyedMessage[String, Array[Byte]](KafkaTopic, tweetEnc)
+    val random = new java.util.Random
+    val tweetEnc = toBinary[Tweet].apply(t)
+    val msg = new KeyedMessage[String, String](KafkaTopic,random.toString ,toJson(t.getSchema).apply(t))
     kafkaProducer.send(msg)
   }
 
